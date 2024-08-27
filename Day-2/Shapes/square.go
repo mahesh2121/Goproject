4e69d2dd8ad4a551440abf9114567b29bff4a662
mahesh2121 <maheshgaikwad128@gmail.com>
2024-08-27 15:02:10 +0530

    shapes

---
+++ Day-2/Shapes/square.go
@@ -0,0 +1,11 @@
+package shapes
+
+func AreaOfSquare(side float64) float64 {
+
+	return side * side 
+}
+
+func perimeterOfSquare(side float64) float64 {
+	return 4 * side
+
+}
---
+++ Day-2/array.go
@@ -0,0 +1,12 @@
+package main
+
+
+import "fmt"
+
+func main() {
+(
+fmt.Println("Arrays"))
+var m [5]int = [5]int{1,2,3,4,5} //array of five integers
+fmt.Println("Value of m=>",m)
+
+}
