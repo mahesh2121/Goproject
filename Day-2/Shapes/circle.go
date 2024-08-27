2dd1f2ba918c0ee4cb6c9748699b035b6167be28
mahesh2121 <maheshgaikwad128@gmail.com>
2024-08-27 14:23:50 +0530

    Day-2

---
+++ Day-2/Shapes/circle.go
@@ -0,0 +1,11 @@
+package main
+
+import "math"
+
+func AreaOfCircle(radius float64) float64 {
+	return math.P1 * redius * redius
+}
+
+func diameterOfCircle(redius float64) float64 {
+	return 2 * redius
+}
