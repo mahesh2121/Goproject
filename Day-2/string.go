5d411c06d52ed6a78f1b26d62328497dad3b7ab7
mahesh2121 <maheshgaikwad128@gmail.com>
2024-08-27 14:55:01 +0530

    string variable

---
+++ Day-2/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("string:")
+	// var n string = "Hello, World!"
+	n := "hello alternative way"
+	// n := "Hello, World!" // this also work as short hand declaration
+	fmt.Println("n=", n)
+}
